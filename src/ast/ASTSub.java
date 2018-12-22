201c955372adf48945acd89ab4a3809c2f02aff1
j.geraldo <j.geraldo@FCT.UNL.PT>
2018-12-22 18:47:13 +0000

    Implement Double subtraction.

--- src/ast/ASTSub.java
+++ src/ast/ASTSub.java
@@ -5,6 +5,7 @@ import itypes.DoubleType;
 import itypes.IType;
 import itypes.IntType;
 import itypes.TypeException;
+import ivalues.IDouble;
 import ivalues.IValue;
 import ivalues.Int;
 
@@ -24,8 +25,18 @@ public class ASTSub extends ASTNodeClass {
 		IValue v1, v2;
 		v1 = left.eval(e);
 		v2 = right.eval(e);
-		
-		return new Int(((Int)v1).getValue() - ((Int)v2).getValue());
+
+		IType t1 = this.left.getType();
+		IType t2 = this.right.getType();
+
+		if(t1 instanceof IntType && t2 instanceof IntType)
+			return new Int(((Int)v1).getValue() - ((Int)v2).getValue());
+		else if(t1 instanceof IntType && t2 instanceof DoubleType)
+			return new IDouble(((Int)v1).getValue() - ((IDouble)v2).getValue());
+		else if(t1 instanceof DoubleType && t2 instanceof IntType)
+			return new IDouble(((IDouble)v1).getValue() - ((Int)v2).getValue());
+		else
+			return new IDouble(((IDouble)v1).getValue() - ((IDouble)v2).getValue());
 	}
 
 	@Override
@@ -36,7 +47,9 @@ public class ASTSub extends ASTNodeClass {
 
 		if(t1 instanceof IntType && t2 instanceof IntType)
 			return (super.nodeType = IntType.getInstance());
-		else if(t1 instanceof DoubleType && t2 instanceof DoubleType)
+		else if((t1 instanceof DoubleType && t2 instanceof DoubleType) ||
+				(t1 instanceof IntType && t2 instanceof DoubleType) ||
+				(t1 instanceof DoubleType && t2 instanceof  IntType))
 			return (super.nodeType = DoubleType.getInstance());
 		else
 			throw new TypeException("-", IntType.getInstance(), IntType.getInstance(), t1, t2);
@@ -47,13 +60,46 @@ public class ASTSub extends ASTNodeClass {
 		String s1 = this.left.compile(env);
 		String s2 = this.right.compile(env);
 
-		return String.format("%s\n%s\n%s\n%s\n%s\n%s\n%s\n",
-				";left + right",
-				";left", s1,
-				";right",
-				s2,
-				"ineg",
-				"iadd"
-		);
+		IType t1 = this.left.getType();
+		IType t2 = this.right.getType();
+
+		if(t1 instanceof IntType && t2 instanceof IntType) {
+			return String.format("%s\n%s\n%s\n%s\n%s\n%s\n%s\n",
+					";left + right",
+					";left", s1,
+					";right",
+					s2,
+					"ineg",
+					"iadd"
+			);
+		} else if(t1 instanceof DoubleType && t2 instanceof DoubleType) {
+			return "new java/lang/Double\n" +
+					"dup\n" +
+					s1 + "\n" +
+					"invokevirtual java/lang/Double/doubleValue()D\n" +
+					s2 + "\n" +
+					"invokevirtual java/lang/Double/doubleValue()D\n" +
+					"dsub\n" +
+					"invokespecial java/lang/Double/<init>(D)V\n";
+		} else if(t1 instanceof IntType && t2 instanceof DoubleType) {
+			return "new java/lang/Double\n" +
+					"dup\n" +
+					s1 + "\n" +
+					"i2d\n" +
+					s2 + "\n" +
+					"invokevirtual java/lang/Double/doubleValue()D\n" +
+					"dsub\n" +
+					"invokespecial java/lang/Double/<init>(D)V\n";
+		} else if(t1 instanceof DoubleType && t2 instanceof IntType) {
+			return "new java/lang/Double\n" +
+					"dup\n" +
+					s1 + "\n" +
+					"invokevirtual java/lang/Double/doubleValue()D\n" +
+					s2 + "\n" +
+					"i2d\n" +
+					"dsub\n" +
+					"invokespecial java/lang/Double/<init>(D)V\n";
+		}
+		return null;
     }
 }
